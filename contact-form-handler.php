bb13457387b977ef02f5caa2d9f297cff7121bb9
Darko <radovicdarko1994@gmail.com>
2018-08-16 16:47:27 +0200

    responsiv- wrap: 960px

---
+++ contact-form-handler.php
@@ -0,0 +1,21 @@
+<?php
+
+   $email_form = 'EasyTutorial@jkj.com';
+
+    $email_subject = "New Form Submission";
+
+    $email_body = "User Name: $name.\n".
+                   "User Email: $email.\n".
+                    "User Message: $message.\n";
+
+
+    $to = "darmior5@gmail.com";
+
+    $headers = "From: $email_from \r\n";
+    $headers = "Reply-to: $visitors_email \r\n";
+
+    mail($to,$email_subject,$email_body,$headers);
+    
+    header("Location: index.html");
+
+?>
