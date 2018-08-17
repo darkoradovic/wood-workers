7eebc823121036be101ccebda00a99c55e43b00a
Darko <radovicdarko1994@gmail.com>
2018-08-17 14:05:22 +0200

    full responsiv

--- js/script.js
+++ js/script.js
@@ -25,4 +25,14 @@
   });
 });
 
+//NAVIGATION
+
+function openNav() {
+    document.getElementById("nav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("nav").style.width = "0";
+}
+
     
